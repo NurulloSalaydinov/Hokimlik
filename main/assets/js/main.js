5d804b67d8eddfee6cf96202d29c90742389f24f
HusanboyZafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2023-05-05 12:18:34 +0500

    fixing bugs in navbar and adding image for language

--- main/assets/js/main.js
+++ main/assets/js/main.js
@@ -8,47 +8,26 @@ header_navigation_item.forEach((element) => {
     element.addEventListener("mouseenter", () => {
         inner_el = element.lastElementChild
         inner_el_first = element.firstElementChild
-        if (inner_el.getBoundingClientRect().height) {
-            inner_el.style.maxHeight = "0px"
-            header_navigation_item.forEach((el) => {
-                el.classList.remove(`active`)
-            })
-        } else {
-            header_navigation_links_wrapper.forEach((el) => {
-                el.style.maxHeight = 0
-            })
-            header_navigation_item_span.forEach(element => {
-                element.classList.remove("active")
-            });
-            setTimeout(() => {
-                inner_el.style.display = "flex"
-                inner_el.style.maxHeight = inner_el.scrollHeight + "px"
-                inner_el_first.classList.add("active")
-            }, 400)
-        }
+        header_navigation_links_wrapper.forEach((el) => {
+            el.style.maxHeight = 0
+        })
+        header_navigation_item_span.forEach(element => {
+            element.classList.remove("active")
+        });
+        setTimeout(() => {
+            inner_el.style.display = "flex"
+            inner_el.style.maxHeight = inner_el.scrollHeight + "px"
+            inner_el_first.classList.add("active")
+        }, 400)
     })
 
     element.addEventListener("mouseleave", () => {
         inner_el = element.lastElementChild
         inner_el_first = element.firstElementChild
-        if (inner_el.getBoundingClientRect().height) {
-            inner_el.style.maxHeight = "0px"
-            header_navigation_item_span.forEach((el) => {
-                el.classList.remove(`active`)
-            })
-        } else {
-            header_navigation_links_wrapper.forEach((el) => {
-                el.style.maxHeight = 0
-            })
-            header_navigation_item_span.forEach(element => {
-                element.classList.remove("active")
-            });
-            setTimeout(() => {
-                inner_el.style.display = "flex"
-                inner_el.style.maxHeight = inner_el.scrollHeight + "px"
-                inner_el_first.classList.add("active")
-            }, 400)
-        }
+        inner_el.style.maxHeight = "0px"
+        header_navigation_item_span.forEach((el) => {
+            el.classList.remove(`active`)
+        })
     })
 })
 
@@ -57,27 +36,18 @@ hero_action_item.forEach((element) => {
         inner_el = element.lastElementChild
         inner_el_first = element.firstElementChild
         let hero_action_posts = document.querySelectorAll(".main-hero_action_posts")
-        if (inner_el.getBoundingClientRect().height) {
-            inner_el.style.maxHeight = `0px`
-            let hero_action_item = document.querySelectorAll(".main-hero_action_item span")
-            hero_action_item.forEach(element => {
-                element.classList.remove("active")
-            });
-        }
-        else {
-            hero_action_posts.forEach((element) => {
-                element.style.maxHeight = '0px'
-            });
-            let hero_action_item = document.querySelectorAll(".main-hero_action_item span")
-            hero_action_item.forEach(element => {
-                element.classList.remove("active")
-            });
-            setTimeout(() => {
-                inner_el.style.display = "flex"
-                inner_el.style.maxHeight = inner_el.scrollHeight + "px"
-                inner_el_first.classList.add("active")
-            }, 400)
-        }
+        hero_action_posts.forEach((element) => {
+            element.style.maxHeight = '0px'
+        });
+        let hero_action_item = document.querySelectorAll(".main-hero_action_item span")
+        hero_action_item.forEach(element => {
+            element.classList.remove("active")
+        });
+        setTimeout(() => {
+            inner_el.style.display = "flex"
+            inner_el.style.maxHeight = inner_el.scrollHeight + "px"
+            inner_el_first.classList.add("active")
+        }, 400)
     })
 
     element.addEventListener("mouseleave", () => {
@@ -108,7 +78,7 @@ header_burger.addEventListener('click', () => {
     }
     else {
         header_navigation_list.style.display = "flex"
-        header_navigation_list.style.maxHeight = header_navigation_list.scrollHeight + 400 + "px"
+        header_navigation_list.style.maxHeight = header_navigation_list.scrollHeight + 450 + "px"
     }
 
     header_navigation_item.forEach((element) => {
@@ -122,27 +92,26 @@ header_burger.addEventListener('click', () => {
     });
 })
 
-document.querySelector('body').addEventListener("click", () => {
-    header_navigation_item.forEach((element) => {
-        inner_el = element.lastElementChild
-        inner_el_first = element.firstElementChild
+// document.querySelector('body').addEventListener("click", () => {
+//     header_navigation_item.forEach((element) => {
+//         inner_el = element.lastElementChild
+//         inner_el_first = element.firstElementChild
+//         inner_el.style.maxHeight = `0px`
+//         inner_el_first.classList.remove("active")
+//         let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
+//         header_navigation_item_span.forEach(element => {
+//             element.classList.remove("active")
+//         });
+//     });
 
-        inner_el.style.maxHeight = `0px`
-        inner_el_first.classList.remove("active")
-        let header_navigation_item_span = document.querySelectorAll(".header_navigation_item span")
-        header_navigation_item_span.forEach(element => {
-            element.classList.remove("active")
-        });
-    });
-
-    hero_action_item.forEach((element) => {
-        inner_el = element.lastElementChild
-        inner_el_first = element.firstElementChild
-        inner_el.style.maxHeight = `0px`
-        inner_el_first.classList.remove("active")
-        let hero_action_posts = document.querySelectorAll(".main-hero_action_posts span")
-        hero_action_posts.forEach(element => {
-            element.classList.remove("active")
-        });
-    });
-})
+//     hero_action_item.forEach((element) => {
+//         inner_el = element.lastElementChild
+//         inner_el_first = element.firstElementChild
+//         inner_el.style.maxHeight = `0px`
+//         inner_el_first.classList.remove("active")
+//         let hero_action_posts = document.querySelectorAll(".main-hero_action_posts span")
+//         hero_action_posts.forEach(element => {
+//             element.classList.remove("active")
+//         });
+//     });
+// })
